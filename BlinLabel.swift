f9cf06eedce21bbef45529da5d4ec5c1c08727f7
何国伟 <mayoho@outlook.com>
2020-05-12 11:46:55 +0800

    add label

---
+++ BlinLabel.swift
@@ -0,0 +1,20 @@
+//
+//  BlinLabel.swift
+//  MYPod
+//
+//  Created by 何国伟 on 2020/5/12.
+//
+
+import UIKit
+
+class BlinLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
